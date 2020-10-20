0e7496217a1a44d6854a969248294d29926792f7
jBachmanKelley <jBachmanKelley@gmail.com>
2020-10-20 01:18:30 -0400

    Created Skeleton Framework

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
 # John Bachman Kelley, 10/19/2020, Artificial Intelligence
 #   The purpose of this code is to predict energy consumption using three different architectures of linear regression
 #   neurons. We have been given 3 days of data, from 8:00 to 5:00 pm with 1-hour intervals.
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+class Neuron1D:
+
+    # In this method, you need to import, normalize, and set object variables
+    def __init__(self, csv_path):
+        raw_data = pd.read_csv(csv_path)
+
+        return
+
+    def train(self):
+        return
+
+    def test(self):
+        return
+
+
+class Neuron2D:
+
+    # In this method, you need to import, normalize, and set object variables
+    def __init__(self, csv_path):
+        raw_data = pd.read_csv(csv_path)
+
+        return
+
+    def train(self):
+        return
+
+    def test(self):
+        return
+
+
+class Neuron3D:
+
+    # In this method, you need to import, normalize, and set object variables
+    def __init__(self, csv_path):
+        raw_data = pd.read_csv(csv_path)
+
+        return
+
+    def train(self):
+        return
+
+    def test(self):
+        return
+
+
+
