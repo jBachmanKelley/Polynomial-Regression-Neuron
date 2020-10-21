938f25e45a18210fb20ca7d33f893847c88ec971
jBachmanKelley <jBachmanKelley@gmail.com>
2020-10-21 00:46:11 -0400

    Minimally Working Solution

--- main.py
+++ main.py
@@ -3,73 +3,218 @@
 #   neurons. We have been given 3 days of data, from 8:00 to 5:00 pm with 1-hour intervals.
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+from matplotlib import pyplot as plt
 
 class Neuron1D:
 
     # In this method, you need to import, normalize, and set object variables
-    def __init__(self, csv_path):
+    def __init__(self, csv_path, max_epoch, epsilon, alpha):
+        self.max_epoch = max_epoch
+        self.TE = 0
+        self.alpha = alpha
+        self.epsilon = epsilon
         self.raw_data = raw_data = np.genfromtxt(csv_path, delimiter=",")
         self.x = x = raw_data[:, 0]
-        self.real_y = y = raw_data[:, 1]
-        self.y = np.empty(x.shape[0])
+        self.real_y = raw_data[:, 1]
+        self.y = np.zeros(x.shape[0])
         self.weights = np.ones(2)
-        self.ones = np.ones(x.shape[0])
+        self.X = np.vstack([np.ones(x.shape[0]), self.x])
         return
 
     def train(self):
+        sign = 1
+        prev_TE = 10000
+
+        for i in range(self.max_epoch):
+            for w in range(2):
+                # Assess the Total Error of Changing
+                self.predict()
+                sigma = (self.real_y - self.y) ** 2
+                TE = sum(sigma)
+
+                # Check for convergence
+                if (TE < 50):
+                    print("Convergence Reached")
+                    return
+
+                # If TE increases, undo change
+                if(TE > prev_TE):
+                    sign *= -1
+                    prev_TE = TE
+
+                # If TE decreases, continue
+                if (TE < prev_TE):
+                    prev_TE = TE
+                self.weights[w] += self.alpha * sign * 1
         return
 
     def test(self):
-        return
+        # Predict
+        self.predict()
+        totalError = 0
+
+        # Assess T.E
+        for i in range(self.X.shape[1]):
+            totalError += (self.y[i] - self.real_y[i])**2
+        print(f"Total Error: {totalError} \nWeights: {self.weights[0]} {self.weights[1]}\n\n")
+        # Plot Results - Still needs the prediction line
+        plt.scatter(self.x, self.y, c='g')
+        plt.scatter(self.x, self.real_y, c='b')
+        plt.show()
+        return totalError
 
     def predict(self):
-        for i in range(self.x):
+        for i in range(self.x.shape[0]):
             # y = mx + b
-            self.y[i] = (self.weights[1] * self.x[i]) + self.weights[0] * self.ones[i]
+            self.y[i] = (self.weights[1] * self.X[1][i]) + self.weights[0]
         return
 
 
 class Neuron2D:
 
     # In this method, you need to import, normalize, and set object variables
-    def __init__(self, csv_path):
-        raw_data = np.genfromtxt(csv_path, delimiter=",")
-        x = raw_data[:, 0]
-        y = raw_data[:, 1]
-        ones = np.ones(x.shape[0])
+    def __init__(self, csv_path, max_epoch, epsilon, alpha):
+        self.max_epoch = max_epoch
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.raw_data = raw_data = np.genfromtxt(csv_path, delimiter=",")
+        self.x = x = raw_data[:, 0]
+        self.real_y = raw_data[:, 1]
+        self.y = np.zeros(x.shape[0])
+        self.weights = np.ones(3)
+        self.X = np.vstack([np.ones(x.shape[0]), self.x, self.x**2])
         return
 
     def train(self):
+        sign = 1
+        prev_TE = 10000
+
+        for i in range(self.max_epoch):
+            # Assess the Total Error
+            self.predict()
+            sigma = (self.real_y - self.y) ** 2
+            TE = sum(sigma)
+
+            # Check for convergence
+            if (TE < 50):
+                print("Convergence Reached")
+                return
+
+            # If TE increases, reverse direction
+            if(TE > prev_TE):
+                sign *= -1
+                prev_TE = TE
+
+            # If TE decreases, continue
+            if (TE < prev_TE):
+                prev_TE = TE
+            self.weights[0] += self.alpha * sign * 1
+            self.weights[1] += self.alpha * sign * sum(self.X[1, :])
+            self.weights[2] += self.alpha * sign * sum(self.X[2, :])
         return
 
     def test(self):
-        return
+        # Predict
+        self.predict()
+        totalError = 0
+
+        # Assess T.E
+        for i in range(self.X.shape[1]):
+            totalError += (self.y[i] - self.real_y[i])**2
+        print(f"Total Error: {totalError} \nWeights: {self.weights[0]} {self.weights[1]} {self.weights[2]}\n\n")
+        # Plot Results - Still needs the prediction line
+        plt.scatter(self.x, self.y, c='g')
+        plt.scatter(self.x, self.real_y, c='b')
+        plt.show()
+        return totalError
 
     def predict(self):
+        for i in range(self.x.shape[0]):
+            # y = mx + b
+            self.y[i] = (self.weights[2] * self.X[2][i]) + (self.weights[1] * self.X[1][i]) + self.weights[0]
         return
+
 
 
 class Neuron3D:
 
     # In this method, you need to import, normalize, and set object variables
-    def __init__(self, csv_path):
-        raw_data = np.genfromtxt(csv_path, delimiter=",")
-        x = raw_data[:, 0]
-        y = raw_data[:, 1]
-        ones = np.ones(x.shape[0])
+    def __init__(self, csv_path, max_epoch, epsilon, alpha):
+        self.max_epoch = max_epoch
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.raw_data = raw_data = np.genfromtxt(csv_path, delimiter=",")
+        self.x = x = raw_data[:, 0]
+        self.real_y = raw_data[:, 1]
+        self.y = np.zeros(x.shape[0])
+        self.weights = np.ones(4)
+        self.X = np.vstack([np.ones(x.shape[0]), self.x, self.x**2, self.x**3])
         return
 
+
     def train(self):
+        sign = 1
+        prev_TE = 10000
+
+        for i in range(self.max_epoch):
+            # Assess the Total Error
+            self.predict()
+            sigma = (self.real_y - self.y) ** 2
+            TE = sum(sigma)
+
+            # Check for convergence
+            if (TE < 50):
+                print("Convergence Reached")
+                return
+
+            # If TE increases, reverse direction
+            if(TE > prev_TE):
+                sign *= -1
+                prev_TE = TE
+
+            # If TE decreases, continue
+            if (TE < prev_TE):
+                prev_TE = TE
+            self.weights[0] += self.alpha * sign * 1
+            self.weights[1] += self.alpha * sign * sum(self.X[1, :])
+            self.weights[2] += self.alpha * sign * sum(self.X[2, :])
         return
 
     def test(self):
-        return
+        # Predict
+        self.predict()
+        totalError = 0
+
+        # Assess T.E
+        for i in range(self.X.shape[1]):
+            totalError += (self.y[i] - self.real_y[i])**2
+        print(f"Total Error: {totalError} \nWeights: {self.weights[0]} {self.weights[1]} {self.weights[2]} {self.weights[3]}\n\n")
+        # Plot Results - Still needs the prediction line
+        plt.scatter(self.x, self.y, c='g')
+        plt.scatter(self.x, self.real_y, c='b')
+        plt.show()
+        return totalError
 
     def predict(self):
+        for i in range(self.x.shape[0]):
+            # y = mx + b
+            self.y[i] = (self.weights[3] * self.X[3][i]) + (self.weights[2] * self.X[2][i]) + (self.weights[1] * self.X[1][i]) + self.weights[0]
         return
 
+
 # Main Script #
-obj1 = Neuron1D('test_data_4.txt')
+obj1 = Neuron1D('training_data.txt', 10000, 0.05, 0.0001)
+obj1.train()
+obj1.test()
+# obj2 = Neuron2D('training_data.txt', 10000, 0.05, 0.000001)
+# obj3 = Neuron3D('training_data.txt', 100000, 0.05, 0.000000001)
+
+
+
+
+
+
+
+
 
 
