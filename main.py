913027df658238f099cf5564f8a0da29171835d2
jBachmanKelley <jBachmanKelley@gmail.com>
2020-10-21 21:56:49 -0400

    Formatting, Labels, and Day 4 Test Script
    Made an optional parameter for weights as well. In order to train,. just pass weights to a constructor and DONt train, run test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 class Neuron1D:
 
     # In this method, you need to import, normalize, and set object variables
-    def __init__(self, csv_path, max_epoch, alpha):
+    def __init__(self, csv_path, max_epoch, alpha, weights=np.ones(2)):
         self.max_epoch = max_epoch
         self.TE = 0
         self.alpha = alpha
@@ -18,7 +18,7 @@ class Neuron1D:
         self.x = (self.x - min(x)) / (max(x) - min(x))
         self.real_y = raw_data[:, 1]
         self.y = np.zeros(x.shape[0])
-        self.weights = np.ones(2)
+        self.weights = weights
         self.X = np.vstack([np.ones(x.shape[0]), self.x])
         return
 
@@ -51,8 +51,12 @@ class Neuron1D:
 
     def plot(self):
         # Plot Results - Still needs the prediction line
-        plt.scatter(self.x, self.y, c='g')
+        plt.plot(self.x, self.y, c='g')
+        plt.title("Neuron Model: Architecture A")
+        plt.xlabel("Normalized Time (hour)")
+        plt.ylabel("Energy Consumption (kW)")
         plt.scatter(self.x, self.real_y, c='b')
+        plt.legend(['Model Prediction', 'Test Data'])
         plt.show()
         return
 
@@ -68,7 +72,7 @@ class Neuron1D:
 class Neuron2D:
 
     # In this method, you need to import, normalize, and set object variables
-    def __init__(self, csv_path, max_epoch, alpha):
+    def __init__(self, csv_path, max_epoch, alpha, weights=np.ones(3)):
         self.max_epoch = max_epoch
         self.TE = 0
         self.alpha = alpha
@@ -77,7 +81,7 @@ class Neuron2D:
         self.x = (self.x - min(x)) / (max(x) - min(x))
         self.real_y = raw_data[:, 1]
         self.y = np.zeros(x.shape[0])
-        self.weights = np.ones(3)
+        self.weights = weights
         self.X = np.vstack([np.ones(x.shape[0]), self.x, self.x ** 2])
         return
 
@@ -110,9 +114,12 @@ class Neuron2D:
         return
 
     def plot(self):
-        # Plot Results - Still needs the prediction line
-        plt.scatter(self.x, self.y, c='g')
+        plt.plot(self.x, self.y, c='g')
+        plt.title("Neuron Model: Architecture B")
+        plt.xlabel("Normalized Time (hour)")
+        plt.ylabel("Energy Consumption (kW)")
         plt.scatter(self.x, self.real_y, c='b')
+        plt.legend(['Model Prediction', 'Test Data'])
         plt.show()
         return
 
@@ -128,7 +135,7 @@ class Neuron2D:
 class Neuron3D:
 
     # In this method, you need to import, normalize, and set object variables
-    def __init__(self, csv_path, max_epoch, alpha):
+    def __init__(self, csv_path, max_epoch, alpha, weights=np.ones(4)):
         self.max_epoch = max_epoch
         self.TE = 0
         self.alpha = alpha
@@ -137,7 +144,7 @@ class Neuron3D:
         self.x = (self.x - min(x)) / (max(x) - min(x))
         self.real_y = raw_data[:, 1]
         self.y = np.zeros(x.shape[0])
-        self.weights = np.ones(4)
+        self.weights = weights
         self.X = np.vstack([np.ones(x.shape[0]), self.x, self.x ** 2, self.x ** 3])
         return
 
@@ -168,13 +175,16 @@ class Neuron3D:
         for i in range(self.x.shape[0]):
             # y = mx + b
             self.y[i] = (self.weights[3] * self.X[3][i]) + (self.weights[2] * self.X[2][i]) + (
-                        self.weights[1] * self.X[1][i]) + self.weights[0]
+                    self.weights[1] * self.X[1][i]) + self.weights[0]
         return
 
     def plot(self):
-        # Plot Results - Still needs the prediction line
-        plt.scatter(self.x, self.y, c='g')
+        plt.plot(self.x, self.y, c='g')
+        plt.title("Neuron Model: Architecture C")
+        plt.xlabel("Normalized Time (hour)")
+        plt.ylabel("Energy Consumption (kW)")
         plt.scatter(self.x, self.real_y, c='b')
+        plt.legend(['Model Prediction', 'Test Data'])
         plt.show()
         return
 
@@ -189,7 +199,33 @@ class Neuron3D:
 
 # Main Script #
 
-# Grid Search to Identify Best Alpha Values
+# Section for Testing Against Day 4 #
+test1m = Neuron1D('training_data.txt', 1000, 0.01)
+test1m.train()
+test1 = Neuron1D('test_data_4.txt', 1000, 0.01, test1m.weights)
+test1.test()
+test1.plot()
+print(f"Test-Error for Architecture A: {test1.TE}\n")
+
+test2m = Neuron2D('training_data.txt', 3000, 0.01)
+test2m.train()
+test2 = Neuron2D('test_data_4.txt', 3000, 0.01, test2m.weights)
+test2.test()
+test2.plot()
+print(f"Test-Error for Architecture B: {test2.TE}\n")
+
+test3m = Neuron3D('training_data.txt', 7500, 0.01)
+test3m.train()
+test3 = Neuron3D('test_data_4.txt', 7500, 0.01, test3m.weights)
+test3.test()
+test3.plot()
+print(f"Test-Error for Architecture C: {test3.TE}\n")
+
+
+
+
+
+# Grid Search to Identify Best Alpha Values #
 
 # Create the Testing Grid
 alpha_arr = np.arange(start=0.01, stop=0.001, step=-0.0005)
@@ -224,7 +260,7 @@ try:
     print(f"Architecture1:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
     obj1.plot()
 except Found:
-    print(f"Architecture1:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
+    print(f"Architecture A:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
     obj1.plot()
 
 # Run Tuning for Architecture 2
@@ -248,7 +284,7 @@ try:
     print(f"Architecture2:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
     obj2.plot()
 except Found:
-    print(f"Architecture2:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
+    print(f"Architecture B:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
     obj2.plot()
 
 # Run Tuning for Architecture 3
@@ -272,5 +308,5 @@ try:
     print(f"Architecture3:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
     obj3.plot()
 except Found:
-    print(f"Architecture3:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
+    print(f"Architecture C:\n    Best Alpha: {best_alpha}\n    Best Epoch: {best_epoch}")
     obj3.plot()
